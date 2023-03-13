aee4b23cbff73b262023f981fb47bbef57c7f7bf
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-03-13 12:20:33 -0700

    Create

---
+++ Bookmarklets/GoGaurdian.lua
@@ -0,0 +1,3 @@
+for (let i = 0; i < 100; i++) {
+  window.open("https://www.google.com/search?q=you+just+got+jebaited&rlz=1CASFKO_enUS1047&oq=you+just+got+jebaited&aqs=chrome.0.0i355i512j46i512j0i390l2j69i64l2.3886j0j7&sourceid=chrome&ie=UTF-8&safe=active&ssui=on");
+}
